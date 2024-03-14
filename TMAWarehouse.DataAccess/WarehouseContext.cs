321d0764a50506b54e7aa86d35c2588e432a225b
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-14 23:08:43 +0100

    Nugets installed and DbContext created

---
+++ TMAWarehouse.DataAccess/WarehouseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMAWarehouse.DataAccess.Entites;
+
+namespace TMAWarehouse.DataAccess;
+
+public class WarehouseContext : DbContext
+{
+    public WarehouseContext(DbContextOptions<WarehouseContext> options) 
+        : base(options)
+    {
+        
+    }
+
+    public DbSet<Coordinator> Coordinators { get; set; }
+
+    public DbSet<Employee> Employees { get; set; }
+
+    public DbSet<Item> Items { get; set; }
+
+    public DbSet<Request> Requests { get; set; }
+}
---
+++ TMAWarehouse.DataAccess/WarehouseContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TMAWarehouse.DataAccess;
+
+public class WarehouseContextFactory : IDesignTimeDbContextFactory<WarehouseContext>
+{
+    public WarehouseContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<WarehouseContext>();
+        optionsBuilder.UseSqlServer("Data Source=LAPTOP-AJM90S2R;Initial Catalog=WarehouseProgram;Integrated Security=True;Encrypt=False");
+        return new WarehouseContext(optionsBuilder.Options);
+    }
+}
