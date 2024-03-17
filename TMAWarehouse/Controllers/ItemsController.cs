7f63e14e92e88a80679dcb7f1701d8849ceadbac
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-17 23:21:09 +0100

    Items controller with GetAllItems endpoint added

---
+++ TMAWarehouse/Controllers/ItemsController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TMAWarehouse.ApplicationServices.API.Domain.Item.GetAllItems;
+
+namespace TMAWarehouse.Controllers;
+
+[ApiController]
+[Route("/Items")]
+public class ItemsController : ApiControllerBase
+{
+    public ItemsController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet]
+    [Route("")]
+    public Task<IActionResult> GetAllItems([FromQuery] GetAllItemsRequest request)
+    {
+        return this.HandleRequest<GetAllItemsRequest, GetAllItemsResponse>(request);
+    }
+}
