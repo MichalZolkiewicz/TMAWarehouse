4455124a6ca1f7f3b89b5c305930545d1a56ec5e
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-17 20:10:16 +0100

    ApplicationServices project added with additional nugets. UserDto class added

---
+++ TMAWarehouse.ApplicationServices/API/Domain/User/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TMAWarehouse.ApplicationServices.API.Domain.User
+{
+    internal class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
