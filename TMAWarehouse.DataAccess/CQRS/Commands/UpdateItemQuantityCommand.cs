2c8be355eaae1dab672cb41e1ddba5e7e2cf853f
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-17 23:20:24 +0100

    update quantity command with command base, and command executor added

---
+++ TMAWarehouse.DataAccess/CQRS/Commands/UpdateItemQuantityCommand.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMAWarehouse.DataAccess.Entites;
+
+namespace TMAWarehouse.DataAccess.CQRS.Commands;
+
+public class UpdateItemQuantityCommand : CommandBase<Item, Item>
+{
+    public override async Task<Item> Execute(WarehouseContext context)
+    {
+        var item = await context.Items.FirstOrDefaultAsync(x => x.Id == this.Parameter.Id);
+        if(item != null) 
+        {
+            item.Quantity = this.Parameter.Quantity;
+        }
+        else
+        {
+            return default;
+        }
+        await context.SaveChangesAsync();
+        return this.Parameter;
+    }
+}
