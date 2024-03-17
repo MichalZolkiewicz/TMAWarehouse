1ad1f5c22e56b43f20de4d935f69f7798694c451
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-17 23:20:01 +0100

    handler for get all items added

---
+++ TMAWarehouse.ApplicationServices/API/Handlers/Items/GetAllItemsHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using TMAWarehouse.ApplicationServices.API.Domain.Item;
+using TMAWarehouse.ApplicationServices.API.Domain.Item.GetAllItems;
+using TMAWarehouse.DataAccess.CQRS;
+using TMAWarehouse.DataAccess.CQRS.Queries;
+
+namespace TMAWarehouse.ApplicationServices.API.Handlers.Items;
+
+public class GetAllItemsHandler : IRequestHandler<GetAllItemsRequest, GetAllItemsResponse>
+{
+    private readonly IMapper _mapper;
+    private readonly IQueryExecutor _queryExecutor;
+
+    public GetAllItemsHandler(IMapper mapper, IQueryExecutor queryExecutor)
+    {
+        _mapper = mapper;
+        _queryExecutor = queryExecutor;
+    }
+
+    public async Task<GetAllItemsResponse> Handle(GetAllItemsRequest request, CancellationToken cancellationToken)
+    {
+        var query = new GetAllItemsQuery();
+        var items = await _queryExecutor.Execute(query);
+        var mappedItems = _mapper.Map<List<ItemDto>>(items);
+        var response = new GetAllItemsResponse()
+        {
+            Data = mappedItems
+        };
+        return response;
+    }
+}
