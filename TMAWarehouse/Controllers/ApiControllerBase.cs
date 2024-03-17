ed93fdc1200b869c5358def356b37c849c577a7b
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-17 23:20:56 +0100

    ApiControllerBase abstract class created

---
+++ TMAWarehouse/Controllers/ApiControllerBase.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TMAWarehouse.Controllers;
+
+public abstract class ApiControllerBase : ControllerBase
+{
+    protected readonly IMediator _mediator;
+
+    protected ApiControllerBase(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    protected async Task<IActionResult> HandleRequest<TRequest, TResponse>(TRequest request)
+        where TRequest : IRequest<TResponse>
+    {
+        var response = await _mediator.Send(request);
+        return this.Ok(response);
+    }
+}
