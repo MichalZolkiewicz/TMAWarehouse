b103ac502a7ac693fc43d8e8729d96933ecccebc
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-17 23:19:06 +0100

    mapping profile for item created

---
+++ TMAWarehouse.ApplicationServices/API/Mappings/ItemsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TMAWarehouse.ApplicationServices.API.Domain.Item;
+using TMAWarehouse.DataAccess.Entites;
+
+namespace TMAWarehouse.ApplicationServices.API.Mappings;
+
+public class ItemsProfile : Profile
+{
+    public ItemsProfile()
+    {
+        this.CreateMap<Item, ItemDto>()
+             .ForMember(x => x.Id, y => y.MapFrom(z => z.Id))
+             .ForMember(x => x.Name, y => y.MapFrom(z => z.Name))
+             .ForMember(x => x.Quantity, y => y.MapFrom(z => z.Quantity))
+             .ForMember(x => x.StorageLocation, y => y.MapFrom(z => z.StorageLocation));
+    }
+}
