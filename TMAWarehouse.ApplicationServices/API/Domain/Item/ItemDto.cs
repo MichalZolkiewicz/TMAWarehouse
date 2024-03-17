81dcb820c0619c70d206c5979c816a8c6edbb4ff
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-17 23:18:49 +0100

    quantity added to dto

--- TMAWarehouse.ApplicationServices/API/Domain/Item/ItemDto.cs
+++ TMAWarehouse.ApplicationServices/API/Domain/Item/ItemDto.cs
@@ -4,6 +4,7 @@ public class ItemDto
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public int Quantity { get; set; }
     public string StorageLocation { get; set; }
 
 }
