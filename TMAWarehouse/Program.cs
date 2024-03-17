9108420db495a3fe4e53ac9880e7c7b36ac7785b
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-17 23:22:18 +0100

    program.cs

--- TMAWarehouse/Program.cs
+++ TMAWarehouse/Program.cs
@@ -1,16 +1,26 @@
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using TMAWarehouse.ApplicationServices.API.Domain;
+using TMAWarehouse.ApplicationServices.API.Mappings;
 using TMAWarehouse.DataAccess;
+using TMAWarehouse.DataAccess.CQRS;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
+builder.Services.AddMediatR(typeof(ResponseBase<>).Assembly);
+builder.Services.AddAutoMapper(typeof(ItemsProfile).Assembly);
+builder.Services.AddTransient<IQueryExecutor, QueryExecutor>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<WarehouseContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("WarehouseConnection")));
+
+
 
 var app = builder.Build();
 
