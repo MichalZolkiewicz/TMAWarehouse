eca5e32535935bc0d17a1cb150a2e256138c06f6
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-18 16:56:15 +0100

    DeleteItem Command created

---
+++ TMAWarehouse.DataAccess/CQRS/Commands/DeleteItemCommand.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMAWarehouse.DataAccess.Entites;
+
+namespace TMAWarehouse.DataAccess.CQRS.Commands;
+
+public class DeleteItemCommand : CommandBase<Item, Item>
+{
+    public int Id { get; set; }
+    public override async Task<Item> Execute(WarehouseContext context)
+    {
+        var item = await context.Items.FirstOrDefaultAsync(x => x.Id == Id);
+        context.Items.Remove(item);
+        await context.SaveChangesAsync();
+        return this.Parameter;
+    }
+}
