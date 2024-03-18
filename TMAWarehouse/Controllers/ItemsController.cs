505eb2b19850a0c7cefe93256ee9f270f1a4fa8e
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-18 16:51:54 +0100

    InvariantGlobalization changed to false

--- TMAWarehouse/Controllers/ItemsController.cs
+++ TMAWarehouse/Controllers/ItemsController.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using TMAWarehouse.ApplicationServices.API.Domain.Item.AddItem;
+using TMAWarehouse.ApplicationServices.API.Domain.Item.DeleteItem;
 using TMAWarehouse.ApplicationServices.API.Domain.Item.GetAllItems;
+using TMAWarehouse.ApplicationServices.API.Domain.Item.UpdateItemQuantity;
 
 namespace TMAWarehouse.Controllers;
 
@@ -17,5 +20,31 @@ public class ItemsController : ApiControllerBase
     public Task<IActionResult> GetAllItems([FromQuery] GetAllItemsRequest request)
     {
         return this.HandleRequest<GetAllItemsRequest, GetAllItemsResponse>(request);
+    }
+
+    [HttpPut]
+    [Route("/QuantityUpdate")]
+    public Task<IActionResult> UpdateItemQuantity([FromBody] UpdateItemQuantityRequest request) 
+    {
+        return this.HandleRequest<UpdateItemQuantityRequest, UpdateItemQuantityResponse>(request);
+    }
+
+    [HttpPost]
+    [Route("/AddItem")]
+    public Task<IActionResult> AddItem([FromBody] AddItemRequest request)
+    {
+        return this.HandleRequest<AddItemRequest, AddItemResponse>(request);
+    }
+
+    [HttpDelete]
+    [Route("/DeleteItem/{itemId}")]
+    public Task<IActionResult> DeleteItem([FromRoute] int itemId)
+    {
+        var request = new DeleteItemRequest
+        {
+            Id = itemId
+        };
+
+        return this.HandleRequest<DeleteItemRequest, DeleteItemResponse>(request);
     }
 }
