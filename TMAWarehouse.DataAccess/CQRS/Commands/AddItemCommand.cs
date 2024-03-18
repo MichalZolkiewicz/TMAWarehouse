573d8670b2babe770ae387a6ffdbc79f70923891
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-18 16:55:04 +0100

    AddItemCommand created

---
+++ TMAWarehouse.DataAccess/CQRS/Commands/AddItemCommand.cs
@@ -0,0 +1,13 @@
+﻿using TMAWarehouse.DataAccess.Entites;
+
+namespace TMAWarehouse.DataAccess.CQRS.Commands;
+
+public class AddItemCommand : CommandBase<Item, Item>
+{
+    public override async Task<Item> Execute(WarehouseContext context)
+    {
+        await context.Items.AddAsync(this.Parameter);
+        await context.SaveChangesAsync();
+        return this.Parameter;
+    }
+}
