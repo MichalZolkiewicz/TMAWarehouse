21419b894c9207f2828c9184f6e00e6fe4f1366d
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-19 00:43:23 +0100

    commands for adding request and updating status created

---
+++ TMAWarehouse.DataAccess/CQRS/Commands/AddRequestCommand.cs
@@ -0,0 +1,29 @@
+﻿using TMAWarehouse.DataAccess.Entites;
+
+namespace TMAWarehouse.DataAccess.CQRS.Commands;
+
+public class AddRequestCommand : CommandBase<Request, Request>
+{
+    public override async Task<Request> Execute(WarehouseContext context)
+    {
+        var user = context.Users.FirstOrDefault(x => x.Name == this.Parameter.UserName);
+        var item = context.Items.FirstOrDefault(x => x.Id == this.Parameter.ItemId);
+
+        var request = new Request()
+        {
+            Id = this.Parameter.Id,
+            User = user,
+            UserName = user.Name,
+            ItemId = item.Id,
+            Item = item,
+            UnitOfMeasurement = this.Parameter.UnitOfMeasurement,
+            Quantity = this.Parameter.Quantity,
+            NetPrice = this.Parameter.NetPrice,
+            Comment = this.Parameter.Comment
+        };
+
+        await context.Requests.AddAsync(request);
+        await context.SaveChangesAsync();
+        return request;
+    }
+}
---
+++ TMAWarehouse.DataAccess/CQRS/Commands/UpdateRequestStatusCommand.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMAWarehouse.DataAccess.Entites;
+
+namespace TMAWarehouse.DataAccess.CQRS.Commands
+{
+    public class UpdateRequestStatusCommand : CommandBase<Request, Request>
+    {
+        public override async Task<Request> Execute(WarehouseContext context)
+        {
+            var request = await context.Requests.FirstOrDefaultAsync(x => x.Id == this.Parameter.Id);
+            var item = await context.Items.FirstOrDefaultAsync(x => x.Id == this.Parameter.ItemId);
+            if (request != null && item != null && item.Quantity > this.Parameter.Quantity) 
+            {
+                request.Status = this.Parameter.Status;
+                item.Quantity = item.Quantity - this.Parameter.Quantity;
+            }
+            
+            await context.SaveChangesAsync();
+            return this.Parameter;
+        }
+    }
+}
