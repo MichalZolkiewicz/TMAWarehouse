5bb5f3f927164d9a1adb26d5a2abe085130bdf1b
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-19 00:43:37 +0100

    controller with post endpoint

---
+++ TMAWarehouse/Controllers/RequestController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TMAWarehouse.ApplicationServices.API.Domain.Request.AddRequest;
+
+namespace TMAWarehouse.Controllers;
+
+[ApiController]
+[Route("/Request")]
+public class RequestController : ApiControllerBase
+{
+    public RequestController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpPost]
+    [Route("/AddRequest")]
+    public Task<IActionResult> AddRequest([FromBody] AddRequestRequest request)
+    {
+        return this.HandleRequest<AddRequestRequest, AddRequestResponse>(request);
+    }
+        
+}
