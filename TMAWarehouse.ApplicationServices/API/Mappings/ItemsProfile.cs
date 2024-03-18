e4d5b54c371eae0f684d5830c2bf822be0458567
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-18 16:55:53 +0100

    ItemProfile modified for Update and Add

--- TMAWarehouse.ApplicationServices/API/Mappings/ItemsProfile.cs
+++ TMAWarehouse.ApplicationServices/API/Mappings/ItemsProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using TMAWarehouse.ApplicationServices.API.Domain.Item;
+using TMAWarehouse.ApplicationServices.API.Domain.Item.AddItem;
+using TMAWarehouse.ApplicationServices.API.Domain.Item.UpdateItemQuantity;
 using TMAWarehouse.DataAccess.Entites;
 
 namespace TMAWarehouse.ApplicationServices.API.Mappings;
@@ -13,5 +15,20 @@ public class ItemsProfile : Profile
              .ForMember(x => x.Name, y => y.MapFrom(z => z.Name))
              .ForMember(x => x.Quantity, y => y.MapFrom(z => z.Quantity))
              .ForMember(x => x.StorageLocation, y => y.MapFrom(z => z.StorageLocation));
+
+        this.CreateMap<UpdateItemQuantityRequest, Item>()
+            .ForMember(x => x.Id, y => y.MapFrom(z => z.Id))
+            .ForMember(x => x.Quantity, y => y.MapFrom(z => z.ItemQuantity));
+
+        this.CreateMap<AddItemRequest, Item>()
+             .ForMember(x => x.Name, y => y.MapFrom(z => z.Name))
+             .ForMember(x => x.ItemGroup, y => y.MapFrom(z => z.ItemGroup))
+             .ForMember(x => x.UnitOfMeasurement, y => y.MapFrom(z => z.UnitOfMeasurement))
+             .ForMember(x => x.Quantity, y => y.MapFrom(z => z.Quantity))
+             .ForMember(x => x.NetPrice, y => y.MapFrom(z => z.NetPrice))
+             .ForMember(x => x.Status, y => y.MapFrom(z => z.Status))
+             .ForMember(x => x.StorageLocation, y => y.MapFrom(z => z.StorageLocation))
+             .ForMember(x => x.ContactPerson, y => y.MapFrom(z => z.ContactPerson));
+
     }
 }
