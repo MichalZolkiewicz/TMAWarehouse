6bf577b4f0c055c00261cc4656d09783dca163f1
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-18 16:53:04 +0100

    UpdateItemRequest and Response added

---
+++ TMAWarehouse.ApplicationServices/API/Domain/Item/UpdateItemQuantity/UpdateItemQuantityRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TMAWarehouse.ApplicationServices.API.Domain.Item.UpdateItemQuantity;
+
+public class UpdateItemQuantityRequest : IRequest<UpdateItemQuantityResponse>
+{
+    public int Id { get; set; }
+    public int ItemQuantity { get; set; }
+}
---
+++ TMAWarehouse.ApplicationServices/API/Domain/Item/UpdateItemQuantity/UpdateItemQuantityResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace TMAWarehouse.ApplicationServices.API.Domain.Item.UpdateItemQuantity;
+
+public class UpdateItemQuantityResponse : ResponseBase<ItemDto>
+{
+
+}
