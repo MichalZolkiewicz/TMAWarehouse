d300f33ed038a38a522667fcc9137b19f20ab3f6
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-18 16:55:24 +0100

    Add and Update Item handlers created

---
+++ TMAWarehouse.ApplicationServices/API/Handlers/Items/AddItemHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using TMAWarehouse.ApplicationServices.API.Domain.Item;
+using TMAWarehouse.ApplicationServices.API.Domain.Item.AddItem;
+using TMAWarehouse.DataAccess.CQRS;
+using TMAWarehouse.DataAccess.CQRS.Commands;
+using TMAWarehouse.DataAccess.Entites;
+
+namespace TMAWarehouse.ApplicationServices.API.Handlers.Items;
+
+internal class AddItemHandler : IRequestHandler<AddItemRequest, AddItemResponse>
+{
+    private readonly IMapper _mapper;
+    private readonly ICommandExecutor _commandExecutor;
+
+    public AddItemHandler(IMapper mapper, ICommandExecutor commandExecutor)
+    {
+        _mapper = mapper;
+        _commandExecutor = commandExecutor;
+    }
+    public async Task<AddItemResponse> Handle(AddItemRequest request, CancellationToken cancellationToken)
+    {
+        var item = _mapper.Map<Item>(request);
+        var command = new AddItemCommand() { Parameter = item };
+        var itemAddedToDb = await _commandExecutor.Execute(command);
+
+        return new AddItemResponse()
+        {
+            Data = _mapper.Map<ItemDto>(itemAddedToDb)
+        };
+    }
+}
---
+++ TMAWarehouse.ApplicationServices/API/Handlers/Items/UpdateItemQuantityHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TMAWarehouse.ApplicationServices.API.Domain.Item;
+using TMAWarehouse.ApplicationServices.API.Domain.Item.UpdateItemQuantity;
+using TMAWarehouse.DataAccess.CQRS;
+using TMAWarehouse.DataAccess.CQRS.Commands;
+using TMAWarehouse.DataAccess.Entites;
+
+namespace TMAWarehouse.ApplicationServices.API.Handlers.Items;
+
+public class UpdateItemQuantityHandler : IRequestHandler<UpdateItemQuantityRequest, UpdateItemQuantityResponse>
+{
+    private readonly IMapper _mapper;
+    private readonly ICommandExecutor _commandExecutor;
+
+    public UpdateItemQuantityHandler(IMapper mapper, ICommandExecutor commandExecutor)
+    {
+        _mapper = mapper;
+        _commandExecutor = commandExecutor;
+    }
+    public async Task<UpdateItemQuantityResponse> Handle(UpdateItemQuantityRequest request, CancellationToken cancellationToken)
+    {
+        var candidate = _mapper.Map<Item>(request);
+        var command = new UpdateItemQuantityCommand() { Parameter = candidate };
+        var updateCandidate = await _commandExecutor.Execute(command);
+        return new UpdateItemQuantityResponse
+        {
+            Data = _mapper.Map<ItemDto>(updateCandidate)
+        };
+    }
+}
