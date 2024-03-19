50edb9246fdb814795ce6ac104d4367ec15545c1
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-19 16:44:02 +0100

    command to add user to db created

---
+++ TMAWarehouse.DataAccess/CQRS/Commands/AddUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using TMAWarehouse.DataAccess.Entites;
+
+namespace TMAWarehouse.DataAccess.CQRS.Commands;
+
+public class AddUserCommand : CommandBase<User, User>
+{
+    public override async Task<User> Execute(WarehouseContext context)
+    {
+        await context.AddAsync(this.Parameter);
+        await context.SaveChangesAsync();
+        return this.Parameter;
+    }
+}
