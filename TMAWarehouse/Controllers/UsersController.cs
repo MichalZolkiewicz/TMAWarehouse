6b810bcba51bd4754d1eb0379a8b7a80ef831e72
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-19 20:34:28 +0100

    UserController created

---
+++ TMAWarehouse/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TMAWarehouse.ApplicationServices.API.Domain.User.AddUser;
+
+namespace TMAWarehouse.Controllers;
+
+[ApiController]
+[Route("/Users")]
+public class UsersController : ApiControllerBase
+{
+    public UsersController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpPost]
+    [Route("/AddUser")]
+    public Task<IActionResult> AddUser([FromBody] AddUserRequest request)
+    {
+        return this.HandleRequest<AddUserRequest, AddUserResponse>(request);
+    }
+}
